2ae1fe114e6416df45ec16a49a98250e35100811
Ruzangithub <118183445+Ruzangithub@users.noreply.github.com>
2023-06-05 09:35:56 +0545

    js2ndclass

---
+++ main.js
@@ -0,0 +1,25 @@
+function AddTwoNum (a,b) 
+{
+ try
+ {
+    if (typeof(a) != 'number')
+    {
+        throw ('type argument is not a number');
+    }
+    else if(typeof(b) !='number') 
+    {
+        throw('type argument is not a number');
+    }
+    else 
+    {
+        console.log(a+b)
+    }
+}  
+ catch(err)
+ {
+    console.log("error occured",err)
+
+ }
+}
+
+AddTwoNum(5,"5");
